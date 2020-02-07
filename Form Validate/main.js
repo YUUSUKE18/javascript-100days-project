26f9154c40a1df2b3e715015e70a5cf75aaebe53
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-02-07 17:04:11 +0900

    Form Validation Project

--- Form Validate/main.js
+++ Form Validate/main.js
@@ -1,71 +1,70 @@
 const form = document.getElementById('form');
-var name = document.getElementById('name');
-var email = document.getElementById('email');
-var pass = document.getElementById('pass');
-var re_pass = document.getElementById('re_pass');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+const re_pass = document.getElementById('re_pass');
 
 function showError(input, message){
     const formControl = input.parentElement;
-    formControl.className = 'form-control error';
+    formControl.className = "form-control error";
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
 function showSuccess(input){
     const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+    formControl.className = "form-control success";
 }
 
-function isVaildEmail(email){
+function checkEmail(input){
     const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return check.test(String(email).toLowerCase());
+    if(check.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not vailded");
+    }
+}
+
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} Check your info`);
+        } else {
+            showSuccess(input);
+        }
+    })
+}
+
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPasswordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, "Please chech your password");
+    }
+}
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    checkRequired([name, email, pass, re_pass]);
 
-    if(name.value === '')
-    {  
-        showError(name, "Please type your name!");
-    } 
-    else 
-    {
-        showSuccess(name);
-    }
-
-    if(email.value === '')
-    {
-        showError(email, "Please type your name!");
-    }
-    else if (!isVaildEmail(email.value))
-    {
-        showError(email, 'Email is not vaild');
-    } 
-    else 
-    {
-        showSuccess(email);
-    }
-
-    if(pass.value === '')
-    {
-        showError(pass, "Please type your name!");
-    } 
-    else 
-    {
-        showSuccess(pass);
-    }
-
-    if(re_pass.value === '')
-    {
-        showError(re_pass, "Please type your name!");
-    } 
-    else 
-    {
-        showSuccess(re_pass);
-    }
+    checkLength(name, 3, 15);
+    checkLength(pass, 8, 32);
+    checkEmail(email);
+    checkPasswordMatch(pass, re_pass);
 })
-
 
 
 // if(name.length < 0){
