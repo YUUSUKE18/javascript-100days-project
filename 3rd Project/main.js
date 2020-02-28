257956e7a0305d5755dc3c6609c09a2641260a95
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-02-28 17:15:25 +0900

    3rd Project commit FEB 28th

---
+++ 3rd Project/main.js
@@ -0,0 +1,8 @@
+const para = document.getElementById('title');
+console.log(para);
+
+const paras = document.getElementsByClassName('error');
+console.log(paras);
+
+const tag = document.getElementsByTagName('p');
+console.log(tag);
