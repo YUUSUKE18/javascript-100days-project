5f5bb7d2f7cd8ebda967785fda38ee956eb1754c
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-06-18 17:54:13 +0900

    Completed Menu List Project

--- Menu List Project/menu.js
+++ Menu List Project/menu.js
@@ -11,7 +11,7 @@ const menu = [
   {
     id: 2,
     title: "Pancake - Simple",
-    category: "breakfast",
+    category: "lunch",
     price: 980,
     img: "./assets/pancake.jpg",
     description:
@@ -20,7 +20,7 @@ const menu = [
   {
     id: 3,
     title: "Pancake - Simple",
-    category: "dinner",
+    category: "lunch",
     price: 980,
     img: "./assets/pancake.jpg",
     description:
@@ -38,7 +38,7 @@ const menu = [
   {
     id: 5,
     title: "Pancake - Simple",
-    category: "TeaTime",
+    category: "lunch",
     price: 980,
     img: "./assets/pancake.jpg",
     description:
@@ -47,69 +47,7 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const containerBtn = document.querySelector(".btn-container");
+
 window.addEventListener("DOMContentLoaded", () => {
-  displayMenuItems(menu);
-  displayMenuButtons();
+  console.log("hello");
 });
-
-function displayMenuItems(menuItems) {
-  let displayMenu = menuItems.map((item) => {
-    //console.log(item);
-    return `
-        <article class="menu-item">
-          <img src=${item.img} class="photo" alt=${item.title} />
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-              <div class="price">${item.price}円</div>
-            </header>
-            <p class="item-text">
-              ${item.description}
-            </p>
-          </div>
-        </article>
-      `;
-  });
-  displayMenu = displayMenu.join("");
-  //console.log(displayMenu);
-  sectionCenter.innerHTML = displayMenu;
-}
-
-function displayMenuButtons() {
-  const categories = menu.reduce(
-    (values, item) => {
-      if (!values.includes(item.category)) {
-        values.push(item.category);
-      }
-      return values;
-    },
-    ["all"]
-  );
-  const categoryBtn = categories
-    .map((category) => {
-      return `
-        <button class="filter-btn" type="button" data-id=${category}>
-            ${category}
-        </button>
-      `;
-    })
-    .join("");
-  containerBtn.innerHTML = categoryBtn;
-  const filterBtn = document.querySelectorAll(".filter-btn");
-  filterBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const category = e.currentTarget.dataset.id;
-      const menuCategory = menu.filter((menuItem) => {
-        if (menuItem.category === category) {
-          return menuItem;
-        }
-      });
-      if (category === "all") {
-        displayMenuItems(menu);
-      } else {
-        displayMenuItems(menuCategory);
-      }
-    });
-  });
-}
