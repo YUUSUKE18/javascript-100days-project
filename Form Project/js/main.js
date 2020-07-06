de5ad525364c24cd86b6e84ee6c72836765067ee
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-07-06 23:00:18 +0900

    Form Project On The Way

---
+++ Form Project/js/main.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  checkForm();
+});
+
+function checkForm() {
+  const nameCheck = name.value.trim();
+  const emailCheck = email.value.trim();
+  const passwordCheck = password.value.trim();
+
+  if (nameCheck === "") {
+    setError(name, "Please Enter Your Name");
+  } else {
+    setSuccess(name);
+  }
+  if (emailCheck === "") {
+    setError(email, "Please Enter Your Email");
+  } else if (!isEmail(emailCheck)) {
+    setError(email, "Please Enter Your Vaild Email");
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordCheck === "") {
+    setError(name, "Please Enter Password");
+  } else {
+    setSuccess(password);
+  }
+}
+
+function setError(input, message) {
+  const formControl = input.parentElement;
+  const errorMsg = formControl.querySelector("#error");
+  formControl.className = "error-msg";
+  errorMsg.innerText = message;
+}
+
+function setSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
